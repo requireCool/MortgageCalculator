5682acfed2e920716cb958f401a98bb16f9ac30e
Wang <1019715750@qq.com>
2024-02-24 15:27:12 +0800

    fix import

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from pywebio.platform.flask import webio_view
-from web import home_page
+from api.web import home_page
 
 app = Flask(__name__)
 app.add_url_rule('/', 'webio_view_1', webio_view(home_page), methods=['GET', 'POST', 'OPTIONS'])
