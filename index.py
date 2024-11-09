57b961ead06ea1b2e17f6e8fa97c403d3d7dd990
requireCool <55184050+requireCool@users.noreply.github.com>
2024-11-09 04:58:45 +0000

    兼容直接启动

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
+import os
 from pywebio.platform.flask import wsgi_app
 from api.web import home_page
 
 app = wsgi_app(home_page)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080))
